0ed70e9ac27f9127bb20a86d3d43456ee3ea79e7
vladyslav-fenchak <vladyslav.fenchak@clariteia.com>
2021-04-05 12:33:56 +0200

    Update post_gen_project.py
    Fix when no command-line interface is specified.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,10 +6,10 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
+    
 
 if __name__ == '__main__':
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('minos', 'cli.py')
+        cli_file = os.path.join('minos', '{{ cookiecutter.project_library }}', 'cli.py')
         remove_file(cli_file)
