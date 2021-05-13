243a26b66665b30c5eac7e476006676240d274d0
andrea-mucci <81492948+andrea-mucci@users.noreply.github.com>
2021-05-13 15:16:49 +0200

    modified structure
    #2 #3

---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,10 @@
+{% for _ in cookiecutter.project_name %}={% endfor %}
+{{ cookiecutter.project_name }}
+{% for _ in cookiecutter.project_name %}={% endfor %}
+
+{{ cookiecutter.project_short_description }}
+
+Credits
+-------
+
+This package was created with [Cookiecutter](https://github.com/audreyr/cookiecutter)  and the [Minos Package](https://github.com/Clariteia/minos-pypackage) project template.
--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -4,10 +4,10 @@
 
 from setuptools import setup, find_namespace_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} ]
@@ -17,18 +17,17 @@ setup_requirements = ['pytest-runner',]
 test_requirements = ['pytest>=3', ]
 
 setup(
-    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
-    author_email='{{ cookiecutter.email }}',
-    python_requires='>=3.5',
+    author="Andrea Mucci",
+    author_email='andrea@clariteia.com',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
@@ -39,6 +38,7 @@ setup(
     },
     {%- endif %}
     install_requires=requirements,
+    long_description_content_type="text/markdown",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='{{ cookiecutter.project_slug }}',
