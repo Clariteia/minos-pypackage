02315271a2014e7d4b2441eaebad36544e155dd4
Sergio García Prado <sergio.garcia@clariteia.com>
2021-09-21 11:59:00 +0200

    ISSUE #? * Remove copyright from python files.

--- {{cookiecutter.project_slug}}/minos/{{cookiecutter.project_library}}/__init__.py
+++ {{cookiecutter.project_slug}}/minos/{{cookiecutter.project_library}}/__init__.py
@@ -1,11 +1,3 @@
-"""
-Copyright (C) 2021 Clariteia SL
-
-This file is part of minos framework.
-
-Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
-"""
-
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
--- {{cookiecutter.project_slug}}/minos/{{cookiecutter.project_library}}/cli.py
+++ {{cookiecutter.project_slug}}/minos/{{cookiecutter.project_library}}/cli.py
@@ -1,11 +1,3 @@
-"""
-Copyright (C) 2021 Clariteia SL
-
-This file is part of minos framework.
-
-Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
-"""
-
 {%- if cookiecutter.command_line_interface|lower == 'argparse' %}
 import argparse
 {%- endif %}
